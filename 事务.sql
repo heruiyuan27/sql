0285d89f710328cef4a82dec3ea4a28b41d49a11
heruiyuan27 <54887880+heruiyuan27@users.noreply.github.com>
2020-04-05 15:08:30 +0800

    Create 事务.sql

---
+++ 事务.sql
@@ -0,0 +1,30 @@
+CREATE TABLE account (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	NAME VARCHAR(10),
+	balance DOUBLE
+);
+-- 添加数据
+INSERT INTO account (NAME, balance) VALUES ('zhangsan', 1000), ('lisi', 1000);
+				
+UPDATE account SET balance =1000 ;
+	
+START TRANSACTION;
+UPDATE account SET balance = balance - 500 WHERE NAME = 'zhangsan';
+UPDATE account SET balance = balance + 500 WHERE NAME = 'lisi';
+SELECT * FROM account;
+COMMIT;
+ROLLBACK;
+
+SELECT @@autocommit;
+
+--
+
+SELECT @@tx_isolation;
+SET GLOBAL TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+SET GLOBAL TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SET GLOBAL TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+START TRANSACTION;
+UPDATE account SET balance = balance - 500 WHERE NAME = 'zhangsan';
+UPDATE account SET balance = balance + 500 WHERE NAME = 'lisi';
+COMMIT;
+ROLLBACK;
